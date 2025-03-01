5a88f28e32d4752297eb7220ff46408d6b2c1b7f
Abhishek1743 <abhisheksagar1113@gmail.com>
2025-03-01 15:07:21 +0530

    Added hello name using query

--- src/main/java/com/example/FirstProject/HelloWebController.java
+++ src/main/java/com/example/FirstProject/HelloWebController.java
@@ -1,15 +1,24 @@
 package com.example.FirstProject;
 
 
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
 public class HelloWebController {
+
     @GetMapping("/hello")
     public String Hello(){
         return "Hello from BridgeLabz";
     }
 
+
+//    This method handles HTTP GET requests for the "/query" endpoint.
+//    It expects a request parameter named "name" and returns a greeting message.
+//    http://localhost:8080/hello/query?name=Abhishek
+    @RequestMapping(value = {"hello/query"}, method = RequestMethod.GET)
+    public String Hello(@RequestParam(value = "name") String name){
+        return "Hello " +name + " from BridgeLabz ";
+
+    }
 }
