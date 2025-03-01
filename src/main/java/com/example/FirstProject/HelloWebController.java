438f76686f59a547268f2cfb85056aada58ae4a1
Abhishek1743 <abhisheksagar1113@gmail.com>
2025-03-01 15:21:49 +0530

    Added Hello name using params

--- src/main/java/com/example/FirstProject/HelloWebController.java
+++ src/main/java/com/example/FirstProject/HelloWebController.java
@@ -17,8 +17,17 @@ public class HelloWebController {
 //    It expects a request parameter named "name" and returns a greeting message.
 //    http://localhost:8080/hello/query?name=Abhishek
     @RequestMapping(value = {"hello/query"}, method = RequestMethod.GET)
-    public String Hello(@RequestParam(value = "name") String name){
-        return "Hello " +name + " from BridgeLabz ";
+    public String Hello(@RequestParam(value = "name") String name) {
+        return "Hello " + name + " from BridgeLabz ";
+    }
 
+
+
+//    This method handles HTTP GET requests for the "/hello/param/{name}" endpoint.
+//    It takes a dynamic value from the URL path using @PathVariable and returns a greeting message.
+//    http://localhost:8080/hello/param/Abhishek
+    @GetMapping("/hello/param/{name}")
+    public String HelloParam(@PathVariable String name){
+        return "Hello " +name + " from BridgeLabz";
     }
 }
